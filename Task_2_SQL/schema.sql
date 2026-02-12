dd4a4e61715095b6b16efb17937bfacba7e0cb7a
akash-7832 <peterakashiya@gmail.com>
2026-02-12 17:51:31 +0530

    Create sales table in schema.sql

--- Task_2_SQL/schema.sql
+++ Task_2_SQL/schema.sql
@@ -1,1 +1,10 @@
+CREATE TABLE sales (
+    sale_id INT AUTO_INCREMENT PRIMARY KEY,
+    sales_person VARCHAR(100),
+    country VARCHAR(50),
+    product VARCHAR(100),
+    amount DECIMAL(10,2),
+    boxes_shipped INT,
+    sale_date DATE
+);
 
