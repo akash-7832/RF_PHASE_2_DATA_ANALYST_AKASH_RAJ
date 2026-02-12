f1990343a84e604e509bf8f51498017bf09effac
akash-7832 <peterakashiya@gmail.com>
2026-02-12 17:51:53 +0530

    Add SQL queries for total sales and top products

--- Task_2_SQL/queries.sql
+++ Task_2_SQL/queries.sql
@@ -1,1 +1,11 @@
+-- Total sales by country
+SELECT country, SUM(amount) AS total_sales
+FROM sales
+GROUP BY country;
+
+-- Top products by revenue
+SELECT product, SUM(amount) AS revenue
+FROM sales
+GROUP BY product
+ORDER BY revenue DESC;
 
